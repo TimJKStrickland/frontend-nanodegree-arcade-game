6cd5d77dff449275ea69ea56f1ab379ddeaf9e49
Tim JK Strickland <timjkstrickland@gmail.com>
2015-11-11 17:32:24 -0800

    Edits 11/11

--- js/app.js
+++ js/app.js
@@ -17,18 +17,29 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = function() {
+    this.sprite = 'images/char-boy.png';
+};
 
+player.update = function(dt){
 
+}
+
+player.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+
 
 
 
